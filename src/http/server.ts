3d223bb7c993a573cc3ceeffe1cf479f68289e3d
Rafael Medeiros <rafaelmedeirosfmm@gmail.com>
2024-09-14 17:58:05 -0400

    feat: Add zod as midleware

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,38 @@
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoal } from '../functions/create-goals'
 import fastify from 'fastify'
+import z from 'zod'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
 const PORT = 3333
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async req => {
+    const { title, desiredWeeklyFrequency } = req.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
 
 app
   .listen({
